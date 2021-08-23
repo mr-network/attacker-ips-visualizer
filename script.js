33484aa4633005048d16f7e9c6ca53f3a204c0e8
MSS POP User <mss@svrop.net>
2021-08-23 18:09:46 +0900

    Make visualizer

---
+++ script.js
@@ -0,0 +1,90 @@
+const date = findGetParameter('date') ?? '2021-08-16';
+document.getElementById('date').value = date;
+
+const perCountryCount = 10;
+const perASCount = 10;
+
+$.ajax({
+    type: "get",
+    url: `https://cdn.jsdelivr.net/gh/mr-network/attacker-ips-report/${date}.json`,
+    dataType: "json",
+}).then(function (data) {
+    drawGraph(data);
+}, function () {
+    document.getElementById('status').innerText = "Failed";
+}
+);
+
+function drawGraph(data) {
+    document.getElementById('ipcount').innerText = data.TotalIPs;
+
+    const countryPieLabels = Object.keys(data.PerCountry).slice(0, perCountryCount).concat('Other');
+    const countryPieDatasTop10 = Object.values(data.PerCountry).slice(0, perCountryCount);
+    const countryPieDatas = countryPieDatasTop10.concat(data.TotalIPs - sumarray(countryPieDatasTop10));
+
+    var countryPieChart = new Chart(
+        document.getElementById('countryPieChart'),
+        {
+            type: 'doughnut',
+            data: {
+                labels: countryPieLabels,
+                datasets: [{
+                    data: countryPieDatas,
+                    hoverOffset: 4
+                }]
+            },
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Blocked IPs per Country'
+                    },
+                }
+            }
+        }
+    );
+
+    const asPieLabel = Object.keys(data.PerAS).slice(0, perASCount).concat('Other');
+    const asPieDatasTop10 = Object.values(data.PerAS).slice(0, perASCount);
+    const asPieDatas = asPieDatasTop10.concat(data.TotalIPs - sumarray(asPieDatasTop10));
+
+    var asPieChart = new Chart(
+        document.getElementById('asPieChart'),
+        {
+            type: 'doughnut',
+            data: {
+                labels: asPieLabel,
+                datasets: [{
+                    data: asPieDatas,
+                    hoverOffset: 4
+                }]
+            },
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Blocked IPs per AS'
+                    },
+                }
+            }
+        }
+    );
+}
+
+function sumarray(a) {
+    return a.reduce(function (s, e) { return s + e; });
+}
+
+// https://stackoverflow.com/a/5448595
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
